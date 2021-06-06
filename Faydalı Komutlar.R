8da5f3d964c02ee04d641770b9f185d9242ddfbb
Ozan Evkaya <68122167+oevkaya@users.noreply.github.com>
2021-06-06 17:28:29 +0300

    Add files via upload

---
+++ Faydalı Komutlar.R
@@ -0,0 +1,381 @@
+# List of R Commands & Functions 
+
+# A -----------------------------------------------------------------------
+
+
+# abline : Add straight lines to plot.
+# abs : Compute the absolute value of a numeric data object.
+# aggregate : Compute summary statistics of subgroups of a data set.
+# all : Check whether all values of a logical vector are TRUE.
+# any : Check whether any values of a logical vector are TRUE.
+# apply : Apply function by rows or columns of a data frame.
+# apropos : Return character vector with names of objects that contain the input.
+# attach : Give access to variables of a data.frame.
+# attr : Return or set a specific attribute of a data object.
+# attributes : Return or set all attributes of a data object.
+# as.Date : Convert character strings to Date class.
+# as.double : Convert integer to double class.
+# as.factor : Convert a data object to the class factor.
+# as.name : Convert character string to name class object.
+# as.numeric : Convert a data object to the class numeric.
+# axis : Create user-defined axis ticks.
+
+# B -----------------------------------------------------------------------
+
+
+# barplot : Create a barplot.
+# boxplot : Create a boxplot.
+# break : Break for-loop in R.
+
+# C -----------------------------------------------------------------------
+
+
+# call : Create objects of the class call.
+# casefold : Translate character to lower or upper case.
+# cat : Return character string in readable format.
+# cbind : Combine vectors, matrices and/or data frames by column.
+# ceiling : Round numeric up to the next higher integer.
+# charmatch : Return position of first match between two data objects.
+# chartr : Translate characters of string to lower and/or upper case.
+# colMeans : Compute the mean of each column of a numeric data frame, matrix or array.
+# colMedians [robustbase] : Compute the median of each column of a numeric matrix.
+# colnames : Retrieve or set the column names of a data frame or matrix.
+# colSums : Compute the sum of each column of a numeric data frame, matrix or array.
+# complete.cases : Return a logical vector that indicates which cases have no missing values.
+# cumsum : Calculate the cumulative sum of the elements of a numeric vector.
+# cut : Convert numeric values into factorial ranges.
+
+# D -----------------------------------------------------------------------
+
+# dbeta : Return corresponding value of beta density.
+# dbern : Return corresponding value of bernoulli PDF.
+# dbinom : Return corresponding value of binomial density.
+# dcauchy : Return corresponding value of cauchy density.
+# dchisq : Return corresponding value of chi-square PDF.
+# density : Draw Kernel Density Plot.
+# deparse : Convert an expression to the character class.
+# detach : Remove the attachment of a data.frame or unload a package.
+# dexp : Return corresponding value of exponential density.
+# df : Return corresponding value of F PDF.
+# dgamma : Return corresponding value of gamma density.
+# dgeom : Return corresponding value of geometric PDF.
+# dhyper : Return corresponding value of hypergeometric PDF.
+# diff : Compute difference between pairs of consecutive elements of a vector.
+# difftime : Calculate the time difference of two date or time objects.
+# dim : Return the dimension (e.g. the number of columns and rows) of a matrix, array or data frame.
+# dir : Return a character vector of file and/or folder names within a directory.
+# dist : Compute distance matrices.
+# dlnorm : Return corresponding value of log normal PDF.
+# dlogis : Return corresponding value of logistic PDF.
+# dnbinom : Return corresponding value of negative binomial density.
+# dnorm : Create standard normal distribution.
+# do.call : Execute function by its name and a list of corresponding arguments.
+# download.file : Download file from internet.
+# dpois : Return value of poisson density.
+# droplevels : Remove unused levels of a factor.
+# dsignrank : Return corresponding PDF value of wilcoxon signedank statistic.
+# dt : Return corresponding value of Student t PDF.
+# dtukey : Return corresponding value of studentized range PDF.
+# dunif : Return corresponding value of uniform PDF.
+# dweibull : Return corresponding value of weibull density.
+# dwilcox : Return corresponding PDF value of wilcoxon rank sum statistic.
+
+# E -----------------------------------------------------------------------
+
+
+# ecdf : Compute the Empirical Cumulative Distribution Function (ECDF).
+# eval : Evaluate an expression and return the result.
+# exists : Check whether an object is defined in the R environment.
+# expression : Create an R object of the expression class.
+
+# F -----------------------------------------------------------------------
+
+
+# find : Return location where objects of a given name can be found.
+# floor : Round numeric down to the next lower integer.
+# format : Encode data objects into common formats.
+
+# G -----------------------------------------------------------------------
+
+
+# get: Search and call a data object.
+# get0: Call an existing data object or return an alternative value.
+# getwd: Return filepath of the current working directory.
+# gregexpr: Search for match of certain character pattern.
+# grep: Search for match of certain character pattern and return indices.
+# grepl: Search for match of certain character pattern and return logical.
+# gsub: Replace all matches in character string.
+
+# H -----------------------------------------------------------------------
+
+
+# heatmap: Create heatmap in Base R.
+# hist: Create histogram.
+
+# I -----------------------------------------------------------------------
+
+
+# ifelse: Select elements conditionally.
+# integrate: Integrate a user-defined function.
+# IQR: Compute the Interquartile Range.
+# is.double: Test whether a data object has the double class.
+# is.na: Return a logical vector or matrix indicating which elements are missing.
+# is.name: Test whether a data object has the class name.
+# is.nan: Return a logical vector or matrix indicating which elements are not a number.
+# is.null: Return a logical value indicating whether a data object is of the data class NULL.
+# is.unsorted: Check whether an input is unsorted.
+
+# J -----------------------------------------------------------------------
+
+
+# jitter  Add noise to a numeric vector.
+# julian: Return the number of days between two date objects.
+
+# L -----------------------------------------------------------------------
+
+
+# lapply: Apply function to all list elements.
+# layout: Specify complex plot arrangements.
+# length: Return the length of data objects such as vectors or lists.
+# libPath: Get or set directory path of installed packages.
+# list.files: List files with specific extension in working directory.
+# load: Load RData workspace file into R.
+# log: Compute logarithms of numeric values.
+# log2: Compute logarithms of numeric values with base of 2.
+# log10: Compute logarithms of numeric values with base of 10.
+# lowess: Compute the lowess smoother.
+
+# M -----------------------------------------------------------------------
+
+# mapply: Apply function to multiple data groups.
+# match: Return position of first match between two data objects.
+# max: Compute the maximum value of a vector or column.
+# mad: Compute the median absolute deviation.
+# mean: Compute the arithmetic mean.
+# median: Compute the median.
+# merge: Merge data frames.
+# message: Generate diagnostic message.
+# mget: Search and call several data objects.
+# min: Compute the minimum value of a vector or column.
+# months: Return the corresponding month of a date.
+
+# N -----------------------------------------------------------------------
+
+
+# na.omit: Remove incomplete cases of a data object.
+# names: Return or set the names of a data object.
+# nchar: Return the number of elements (e.g. letters) of a character object.
+# ncol: Return the number of columns of a matrix or data frame.
+# next: Skip certain parts of for-loop in R.
+# nrow: Return the number of rows of a matrix or data frame.
+
+# O -----------------------------------------------------------------------
+
+
+# optim: Apply general-purpose optimization.
+# optimize: Perform one dimensional optimization.
+# order: Return position of each input element in ascending or descending order.
+# outer: Apply a function to two arrays.
+
+# P -----------------------------------------------------------------------
+
+
+# packageVersion: Find out the currently loaded version of an R package.
+# pairs: Return a plot matrix consisting of scatterplots for each variable-combination of a data frame.
+# par: Set or Query graphical parameters.
+# parse: Convert a character to the expression class.
+# paste: Concatenate inputs to string.
+# paste0: Concatenate inputs to string with given separator.
+# pbern: Return corresponding value of bernoulli CDF.
+# pbeta: Return corresponding value of beta cumulative distribution function.
+# pbinom: Return corresponding value of binomial cumulative distribution function.
+# pcauchy: Return corresponding value of cauchy cumulative distribution function.
+# pchisq: Return corresponding value of chi-square CDF.
+# pexp: Return corresponding value of exponential cumulative distribution function.
+# pf: Return corresponding value of F CDF.
+# pgamma: Return corresponding value of gamma cumulative distribution function.
+# pgeom: Return corresponding value of geometric CDF.
+# phyper: Return corresponding value of hypergeometric CDF.
+# plnorm: Return corresponding value of log normal CDF.
+# plogis: Return corresponding value of logistic CDF.
+# plot: Draw a scatterplot or density plot.
+# pmatch: Return position of first match between two data objects.
+# pmax: Return the parallel maxima of two or more vectors.
+# pmin: Return the parallel minima of two or more vectors.
+# pnbinom: Return corresponding value of negative binomial cumulative distribution function.
+# pnorm: Return value of distribution function.
+# polygon: Draw a polygon to a plot.
+# ppois: Return value of poisson cumulative distribution function.
+# pretty: Compute a sequence of equally spaced round values.
+# print: Return data object to the R (or RStudio) console.
+# psignrank: Return corresponding CDF value of wilcoxon signedank statistic.
+# pt: Return corresponding value of Student t CDF.
+# ptukey: Return corresponding value of studentized range CDF.
+# punif: Return corresponding value of uniform CDF.
+# pweibull: Return corresponding value of weibull CDF.
+# pwilcox: Return corresponding CDF value of wilcoxon rank sum statistic.
+
+
+# Q -----------------------------------------------------------------------
+
+
+# qbern: Return corresponding value of bernoulli quantile function.
+# qbeta: Return corresponding value of beta quantile function.
+# qbinom: Return corresponding value of binomial quantile function.
+# qcauchy: Return corresponding value of cauchy quantile function.
+# qchisq: Return corresponding value of chi-square quantile function.
+# qexp: Return corresponding value of exponential quantile function.
+# qf: Return corresponding value of F quantile function.
+# qgamma: Return corresponding value of gamma quantile function.
+# qgeom: Return corresponding value of geometric quantile function.
+# qhyper: Return corresponding value of hypergeometric quantile function.
+# qlnorm: Return corresponding value of log normal quantile function.
+# qlogis: Return corresponding value of logistic quantile function.
+# qnbinom: Return corresponding value of negative binomial quantile function.
+# qnorm: Return value of quantile function.
+# qpois: Return value of poisson quantile function.
+# qqnorm: Create a normal QQplot.
+# qsignrank: Return corresponding quantile function value of wilcoxon signedank statistic.
+# qt: Return corresponding value of Student t quantile function.
+# qtukey: Return corresponding value of studentized range quantile function.
+# quantile: Compute sample quantiles.
+# quarters: Return the corresponding quarter of a date.
+# qunif: Return corresponding value of uniform quantile function.
+# qweibull: Return corresponding value of weibull quantile function.
+# qwilcox: Return corresponding quantile function value of wilcoxon rank sum statistic.
+
+# R -----------------------------------------------------------------------
+
+
+# R.Version: Get current R version.
+# rank: Return ranking position of input.
+# rbern: Return bernoulli distributed random number.
+# rbeta: Draw random number from beta density.
+# rbind: Combine vectors, matrices and/or data frames by row.
+# rbinom: Draw random number from binomial density.
+# rcauchy: Draw random number from cauchy density.
+# rchisq: Return chi-square distributed random number.
+# read_excel [readxl Package]: Read Excel File in R.
+# read.xlsx [xlsx Package]: Read Excel File in R.
+# read.xlsx [openxlsx Package]: Read Excel File in R.
+# readline: Interactively read a line from the terminal.
+# readLines: Read text lines from an input file.
+# readRDS: Load RData workspace file into R.
+# regexpr: Search for match of certain character pattern.
+# regexec: Search for match of certain character pattern.
+# remove: Clear specific data object from R workspace.
+# rm: Clear specific data object from R workspace.
+# rep: Replicate elements of vectors and lists.
+# replace: Exchanges values in data object.
+# return: Return output of user-defined R function.
+# rev: Return a reversed version of vectors or other data objects.
+# rexp: Draw random number from exponential density.
+# rf: Return F distributed random number.
+# rgamma: Draw random number from gamma density.
+# rgeom: Return geometrically distributed random number.
+# rhyper: Return hypergeometrically distributed random number.
+# rlnorm: Return log normally distributed random number.
+# rlogis: Return logistically distributed random number.
+# rnbinom: Draw random number from negative binomial density.
+# rnorm: Draw normally distributed random number.
+# round: Round numeric to specified number of decimal places.
+# row.names: Get or set row names of data frame or matrix.
+# rowMeans: Compute the mean of each row of a numeric data frame, matrix or array.
+# rowSums: Compute the sum of each row of a numeric data frame, matrix or array.
+# rpois: Draw random value of poisson density.
+# rsignrank: Return random number according to wilcoxon signedank statistic.
+# rt: Return Student t distributed random number.
+# rtukey: Return studentized range distributed random number.
+# runif: Return uniformly distributed random number.
+# rweibull: Draw random value of weibull density.
+# rwilcox: Return random number according to wilcoxon rank sum statistic.
+
+# S -----------------------------------------------------------------------
+
+
+# sample: Take a random sample or permutation of a data object.
+# sapply: Apply function to all list elements and convert output to vector.
+# save: Save R data objects as RData workspace file.
+# save.image: Save global R environment as RData workspace file.
+# saveRDS: Save single R data object as RData workspace file.
+# scale: Standardize data.
+# scan: Read data into vector or list from file or R console.
+# sd: Compute standard deviation.
+# segments: Draw a line segment between two pairs of points.
+# seq: Generate sequence of numeric values.
+# set.seed: Set a random seed.
+# setdiff: Identify which elements of a data object X are not existent in a data object Y.
+# setNames [stats]: Set names of a data object and return the object.
+# setnames [data.table]: Change names of a data.frame or data.table by reference.
+# setwd: Specify new working directory.
+# shapiro.test: Perform Shapiro-Wilk normality test.
+# sign: Return the signs of numeric elements.
+# signif: Round numeric to specified number of digits.
+# sink: Export R output to external file.
+# solve: Solve system of equations.
+# sort: Return input in ascending or descending order.
+# sort.int: Return input in ascending or descending order.
+# sort.list: Return position of each element of a single input.
+# split: Divide data into groups.
+# sprintf: Return character containing a formatted combination of input values.
+# sqrt: Compute the square root of a numeric data object.
+# stop: Generate error message and stop executing the current R code.
+# strftime: Convert time objects to characters.
+# strptime: Convert characters to time objects.
+# strsplit: Split elements of a character string.
+# structure: Set additional attributes of a data object.
+# sub: Replace first match in character string.
+# substr: Extract or replace a substring in a character vector.
+# substring: Extract or replace a substring in a character vector.
+# sum: Compute the sum of a numeric input vector.
+# summary: Compute summary statistics of data and model objects.
+# sweep: Apply an operation (e.g. + or -) to a matrix by row or column.
+# switch: Run a specific code block and return its result.
+
+# T -----------------------------------------------------------------------
+
+
+# t: Transpose data frame.
+# tapply: Apply function to multiple data groups.
+# text: Draw text elements to plots.
+# tolower: Translate character to lower case.
+# toupper: Translate character to upper case.
+# transform: Modify data frames easily.
+# trimws: Trim Leading and Trailing Whitespace.
+# trunc: Cut off decimal places of a numeric input.
+# tryCatch: Check expressions for error and warning messages.
+# type.convert: Convert data object to appropriate class.
+
+# U -----------------------------------------------------------------------
+
+
+# union: Return values that appear in at least one of two data objects.
+# unique: Remove duplicates from data objects.
+# unlist: Convert a list to a single vector.
+# unsplit: Reverse output of split function.
+
+# V -----------------------------------------------------------------------
+
+
+# vapply: Apply function to all list elements and convert output to vector.
+# var: Compute sample variance.
+
+# W -----------------------------------------------------------------------
+
+
+# warning: Generate warning message.
+# weekdays: Return the corresponding weekday of a date.
+# weighted.mean: Compute the weighted arithmetic mean.
+# which: Return indices of certain values based on logical condition.
+# with: Evaluates expression in environment constructed based on data frame.
+# within: Evaluates expression in environment and modify data frame.
+# write: Write data to files.
+
+# X -----------------------------------------------------------------------
+
+# xtfrm: Produce numeric vector which sorts in same order as input.
+
+
+
+
+
